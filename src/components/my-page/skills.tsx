c0c010d5f015db30de2ef76f97df98f90f08fda2
leibaio <leibaio@qq.com>
2025-06-07 23:38:39 +0800

    update

--- src/components/my-page/skills.tsx
+++ src/components/my-page/skills.tsx
@@ -7,18 +7,15 @@ export default function Skills() {
       <SectionHeader icon={Wrench} title={"专业技能"} />
       <ul className="flex flex-col list-disc list-inside space-y-2">
         <li>
-          熟练掌握 HTML5、CSS3、Less、ES6+、TypeScript 等，快速还原需求页面
+          熟练掌握	HTML5、CSS3、Less、ES6+、TypeScript、RESTful	API、屏幕适配、浏览器渲染、异步编程、跨域等
         </li>
         <li>
-          熟练掌握 Vue2/3 (Vue-Router、Pinia、Vuex
-          等)、React（Hooks、React-Router 等）
+          熟练掌握	Vue2/3	(Vue-Router、Pinia、Vuex	等)、React（Hooks、React-Router	等），快速实现项目搭建
         </li>
         <li>
-          熟练掌握常用样式库如
-          ElementUI、AntDesign、TailwindCSS、ShadcnUI、Echarts
-          等，并能够二次封装
+          熟练掌握常用样式库如	ElementUI、AntDesign、TailwindCSS、ShadcnUI、Echarts	等，并能够二次封装
         </li>
-        <li>熟悉 Webpack、Vite 等前端工程化工具，完成日常配置开发构建</li>
+        <li>熟悉	Webpack、Vite	等前端工程化工具，完成日常配置优化例如	Tree	Shaking	等方案减少打包体积</li>
         <li>熟悉 Linux 系统相关操作命令，在 Nginx 等服务器部署前端项目</li>
         <li>注重用户体验，熟悉响应式设计布局</li>
         <li>了解前端性能优化如懒加载、虚拟列表、异步上传队列等</li>
